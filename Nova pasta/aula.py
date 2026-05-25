7c4c161e5e6fc840c66e8c356472769a6b1f5547
Lucas <lucas.parpinelli@edu.senai.br>
2026-05-25 08:33:26 -0300

    acrecio soma

---
+++ Nova pasta/aula.py
@@ -0,0 +1,8 @@
+fruta=['maça','banana','abacaxi','uva','morango','melancia']
+maior=max(fruta,key=len)
+menor=min(fruta,key=len)
+print(f'maior:{maior}')
+print(f'menor:{menor}')
+#num=[7,4,4,8,4,6,9,5,8,2]
+#num.sort(reverse=True)
+#print(num)
